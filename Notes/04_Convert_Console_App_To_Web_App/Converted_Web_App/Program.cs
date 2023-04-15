8f5f67fd2d6aa79c3aa9462216af74e6e90df206
Roman-Ojha <razzroman98@gmail.com>
2023-04-15 16:08:31 +0545

    feat: init project for Convert console app into Web app & some docs

---
+++ Notes/04_Convert_Console_App_To_Web_App/Converted_Web_App/Program.cs
@@ -0,0 +1,35 @@
+﻿using Converted_Web_App;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConvertedWebApp
+{
+
+    class Program
+    {
+        // Main is the function that get called when starting the application
+        // Also web application can have a program class which will have the 'Main' method
+        // and that main method is the entry point for any .NET Core application
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // We will call the HostBuild while starting of the application
+            // Where we can pass some command line arguments as well
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        // Now we will create the HostBuilder Method
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Now here we will create the default build
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                // we can see the functionality this method have from given image in this directory
+                // Where this function also add Middleware into the application
+                // Setup Middleware now, for that we will create the another class called 'Startup.cs'
+                webBuilder.UseStartup<Startup>();
+                // Now we have to make some changes in Startup class
+            });
+        }
+    }
+}
---
+++ Notes/04_Convert_Console_App_To_Web_App/Converted_Web_App/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converted_Web_App
+{
+    internal class Startup
+    {
+        public void ConfigurationServices()
+        {
+
+        }
+
+        public void Configure()
+        {
+            // this method is use as a middleware into the application
+        }
+    }
+}
