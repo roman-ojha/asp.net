97c176eb29315f43993ab6734855bf3ee4706fdf
Roman-Ojha <razzroman98@gmail.com>
2023-05-25 23:33:52 +0545

    feat(college): init project for lab 03 CRUD & statemamagement

---
+++ College/Lab/03_CRUD_and_StateManagement/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _03_CRUD_and_StateManagement.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
