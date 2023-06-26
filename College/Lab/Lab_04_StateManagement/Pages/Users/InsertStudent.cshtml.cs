4a8afd78af319a66d57fc75e8ad1d4da363beedc
Roman-Ojha <razzroman98@gmail.com>
2023-06-26 18:25:50 +0545

    feat(college lab): complated state management using session, cookies, querystring, viewdata

--- College/Lab/Lab_04_StateManagement/Pages/Users/InsertStudent.cshtml.cs
+++ College/Lab/Lab_04_StateManagement/Pages/Users/InsertStudent.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace Lab_04_StateManagement.Pages.Users
 {
@@ -27,7 +28,15 @@ namespace Lab_04_StateManagement.Pages.Users
 
         public void OnPost()
         {
+            // Store inside session
+            HttpContext.Session.SetString("FirstName", FirstName);
+            HttpContext.Session.SetString("LastName", LastName);
+            HttpContext.Session.SetString("Eamil", Email);
 
+            // Store inside Cookie
+            Response.Cookies.Append("FirstName", FirstName);
+            Response.Cookies.Append("LastName", LastName);
+            Response.Cookies.Append("Email", Email);
         }
     }
 }
