12a7babceed5b0bc34f01ac67b9658feee28c91e
Roman-Ojha <razzroman98@gmail.com>
2023-06-26 21:15:34 +0545

    feat(college lab): init project for role based authentication

---
+++ College/Lab/Lab_06_Role_Based_Authentication/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_06_Role_Based_Authentication.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
