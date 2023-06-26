fe1cfa818f7e11f70e12e74f84d6ee28736637c5
Roman-Ojha <razzroman98@gmail.com>
2023-06-26 23:15:37 +0545

    feat(college lab): completed role based authorization

---
+++ College/Lab/Role_Based_Authorization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Role_Based_Authorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
