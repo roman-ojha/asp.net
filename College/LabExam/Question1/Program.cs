bb63935d07261381c62c385c66a7ae328ca10d5a
roman-ojha <razzroman98@gmail.com>
2023-09-03 15:32:21 +0545

    feat(college): lab exam LINQ & jquery problem solved

---
+++ College/LabExam/Question1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Question1
+{
+    class Program
+    {
+        class User
+        {
+            public string FirstName { get; set; } = "";
+            public string LastName { get; set; } = "";
+            public string Address { get; set; } = "";
+
+            public User(string firstName, string lastName, string address)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Address = address;
+            }
+
+            public static void ShowUser(User user)
+            {
+                Console.WriteLine("First Name: " + user.FirstName);
+                Console.WriteLine("Last Name: " + user.LastName);
+                Console.WriteLine("Address Name: " + user.Address);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("For User " + i);
+                Console.WriteLine("Enter FirstName: ");
+                string fname = Console.ReadLine();
+                Console.WriteLine("Enter LastName: ");
+                string lname = Console.ReadLine();
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+                User user = new User(fname, lname, address);
+                users.Add(user);
+            }
+
+
+            var userResult = from rUser in users
+                             where rUser.Address == "pokhara"
+                             orderby rUser.LastName descending
+                             select rUser;
+
+            foreach(User user in userResult)
+            {
+                User.ShowUser(user);
+            }
+                                    
+        }
+    }
+}
---
+++ College/LabExam/Question2/Pages/JQuery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Question2.Pages
+{
+    public class JQueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
